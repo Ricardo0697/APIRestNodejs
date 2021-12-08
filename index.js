1e6c7409f2e31dfba1ac2757ddf3d15b86228d35
Ricardo0697 <37809835+Ricardo0697@users.noreply.github.com>
2021-12-08 14:43:38 -0600

    Update index.js

--- index.js
+++ index.js
@@ -1,8 +1,18 @@
 const express = require("express");
 const bodyParser = require('body-parser');
 const app = express();
+methodOverride = require("method-override");
+mongoose = require("mongoose");
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(bodyParser.json());
+app.use(methodOverride());
+
+var router = express.Router();
+
+router.get("/", function (req, res) {
+    res.send("Hello World!");
+});
+
 let Car = {
     id: 1,
     year: 2019,
@@ -141,6 +151,59 @@ app.use(function (req, res, next) {
         mensaje: 'URL no encontrada'
     };
     res.status(404).send(respuesta);
+});
+
+exports.addTVShow = function (req, res) {
+    console.log("POST");
+    console.log(req.body);
+
+    var tvshow = new TVShow({
+        title: req.body.title,
+        year: req.body.year,
+        country: req.body.country,
+        poster: req.body.poster,
+        seasons: req.body.seasons,
+        genre: req.body.genre,
+        summary: req.body.summary,
+    });
+
+    tvshow.save(function (err, tvshow) {
+        if (err) return res.status(500).send(err.message);
+        res.status(200).jsonp(tvshow);
+    });
+};
+exports.updateTVShow = function (req, res) {
+    TVShow.findById(req.params.id, function (err, tvshow) {
+        tvshow.title = req.body.petId;
+        tvshow.year = req.body.year;
+        tvshow.country = req.body.country;
+        tvshow.poster = req.body.poster;
+        tvshow.seasons = req.body.seasons;
+        tvshow.genre = req.body.genre;
+        tvshow.summary = req.body.summary;
+
+        tvshow.save(function (err) {
+            if (err) return res.status(500).send(err.message);
+            res.status(200).jsonp(tvshow);
+        });
+    });
+};
+exports.deleteTVShow = function (req, res) {
+    TVShow.findById(req.params.id, function (err, tvshow) {
+        tvshow.remove(function (err) {
+            if (err) return res.status(500).send(err.message);
+            res.status(200).send();
+        });
+    });
+};
+app.use(router);
+mongoose.connect("mongodb://localhost/tvshows", function (err, res) {
+    if (err) {
+        console.log("ERROR: connecting to Database. " + err);
+    }
+    app.listen(3000, function () {
+        console.log("Node server running on http://localhost:3000");
+    });
 });
 app.listen(3000, () => {
     console.log("El servidor está inicializado en el puerto 3000");
